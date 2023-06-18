8b52e6aa46e66cc558b38227d0731f3b2184bea0
YotioSoft <ytani0323@gmail.com>
2023-06-18 12:11:48 +0900

    add FillStructureFromMemory()

--- wincrate/src/main.rs
+++ wincrate/src/main.rs
@@ -1,24 +1,33 @@
-use winapi::shared::ntdef::{ HRESULT, NTSTATUS, NT_SUCCESS };
 use windows::Win32::System::WindowsProgramming::SYSTEM_PROCESS_INFORMATION;
+use windows::Win32::Foundation::HANDLE;
+use winapi::shared::ntdef::{ HRESULT, NTSTATUS, NT_SUCCESS };
 use winapi::ctypes::*;
 use winapi::um::memoryapi::*;
 use winapi::um::processthreadsapi::*;
 use ntapi::ntexapi::*;
 
+fn FillStructureFromMemory<T>(struct_ptr: &mut T, memory_ptr: *const c_void, process_handle: *mut c_void) {
+    unsafe {
+        let mut bytes_read: usize = 0;
+        let res = ReadProcessMemory(process_handle, memory_ptr, struct_ptr as *mut _ as *mut c_void, std::mem::size_of::<T>(), &mut bytes_read);
+        println!("res: {:x}", res);
+        println!("bytes_read: {}", bytes_read);
+    }
+}
+
 fn main() {
     unsafe {
         let mut info_length: u32 = 0x10000;
+        let mut baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
+
         let mut tries = 0;
-
         while true {
-            let baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
-
             let res = NtQuerySystemInformation(0x5, baseaddress, info_length, &mut info_length);
 
             println!("res: {:x}", res);
             println!("info_length: {}", info_length);
             println!("tries: {}", tries);
-            
+
             if res == 0 {
                 break;
             }
@@ -28,9 +37,19 @@ fn main() {
 
             VirtualFree(baseaddress, 0x0, 0x8000);
 
+            baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
+
             tries += 1;
         }
 
-        //println!("baseaddress: {:x?}", baseaddress);
+        println!("baseaddress: {:x?}", baseaddress);
+
+        let mut spi = SYSTEM_PROCESS_INFORMATION::default();    // from windows crate
+        let a = GetCurrentProcess();
+        FillStructureFromMemory(&mut spi, baseaddress as *const c_void, GetCurrentProcess());
+
+        println!("{:#x?}", spi);
+
+        VirtualFree(baseaddress, 0x0, 0x8000);
     }
 }
