7e208d4fa202e68a90673c19d9283e6ebef96ebd
YotioSoft <ytani0323@gmail.com>
2023-06-18 11:52:05 +0900

    get in loop

--- wincrate/src/main.rs
+++ wincrate/src/main.rs
@@ -8,12 +8,29 @@ use ntapi::ntexapi::*;
 fn main() {
     unsafe {
         let mut info_length: u32 = 0x10000;
-        let baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
-    
-        let res = NtQuerySystemInformation(0x5, baseaddress, info_length, &mut info_length);
+        let mut tries = 0;
 
-        println!("res: {}", res);
+        while true {
+            let baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
 
-        VirtualFree(baseaddress, 0x0, 0x8000);
+            let res = NtQuerySystemInformation(0x5, baseaddress, info_length, &mut info_length);
+
+            println!("res: {:x}", res);
+            println!("info_length: {}", info_length);
+            println!("tries: {}", tries);
+            
+            if res == 0 {
+                break;
+            }
+            if tries == 5 {
+                break;
+            }
+
+            VirtualFree(baseaddress, 0x0, 0x8000);
+
+            tries += 1;
+        }
+
+        //println!("baseaddress: {:x?}", baseaddress);
     }
 }
