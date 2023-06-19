c597a218d2453eba7ce319d38ab0efdc356480cb
YotioSoft <ytani0323@gmail.com>
2023-06-19 22:17:03 +0900

    add gethandles

---
+++ gethandles/src/main.rs
@@ -0,0 +1,95 @@
+use windows::Win32::System::WindowsProgramming::SYSTEM_PROCESS_INFORMATION;
+use winapi::shared::ntdef::{ HRESULT, NTSTATUS, NT_SUCCESS };
+use winapi::ctypes::*;
+use winapi::um::memoryapi::*;
+use winapi::um::processthreadsapi::*;
+use winapi::um::winnt::{HANDLE,
+    MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_EXECUTE_READWRITE, PROCESS_ALL_ACCESS
+};
+use ntapi::ntexapi::*;
+
+fn FillStructureFromMemory<T>(struct_ptr: &mut T, memory_ptr: *const c_void, process_handle: *mut c_void) {
+    unsafe {
+        let mut bytes_read: usize = 0;
+        let res = ReadProcessMemory(process_handle, memory_ptr, struct_ptr as *mut _ as *mut c_void, std::mem::size_of::<T>(), &mut bytes_read);
+        println!("res: {:x}", res);
+        println!("bytes_read: {}", bytes_read);
+    }
+}
+
+fn main() {
+    unsafe {
+        let mut info_length: u32 = 0x10000;
+        let mut baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
+
+        let mut tries = 0;
+        while true {
+            let res = NtQuerySystemInformation(0x10, baseaddress, info_length, &mut info_length);
+
+            println!("res: {:x}", res);
+            println!("info_length: {}", info_length);
+            println!("tries: {}", tries);
+
+            if res == 0 {
+                break;
+            }
+            if tries == 5 {
+                break;
+            }
+
+            VirtualFree(baseaddress, 0x0, 0x8000);
+
+            baseaddress = VirtualAlloc(std::ptr::null_mut(), info_length as usize, 0x1000, 0x40);
+
+            tries += 1;
+        }
+
+        println!("baseaddress: {:x?}", baseaddress);
+
+        let mut spi = SYSTEM_PROCESS_INFORMATION::default();    // from windows crate
+        let a = GetCurrentProcess();
+        FillStructureFromMemory(&mut spi, baseaddress as *const c_void, GetCurrentProcess());
+
+        println!("next entry offset: {}", spi.NextEntryOffset);
+        println!("process handle: {:#x?}", spi.UniqueProcessId);
+        println!("image name: {:#x?}", spi.ImageName);
+
+        while true {
+            if spi.NextEntryOffset == 0 {
+                break;
+            }
+
+            let mut previous_addres = baseaddress;
+            let mut next_address = baseaddress as isize + spi.NextEntryOffset as isize;
+
+            FillStructureFromMemory(&mut spi, next_address as *const c_void, GetCurrentProcess());
+            
+            let mut v1: Vec<u16> = vec![0; spi.ImageName.Length as usize];
+            ReadProcessMemory(GetCurrentProcess(), spi.ImageName.Buffer.0 as *const c_void, v1.as_mut_ptr() as *mut c_void, spi.ImageName.Length as usize, std::ptr::null_mut());
+
+            let proc_name = String::from_utf16_lossy(&v1).trim_matches(char::from(0)).to_string();
+            let proc_id = spi.UniqueProcessId;
+            
+            println!("---------------------");
+            println!("next entry offset: {}", spi.NextEntryOffset);
+            println!("process handle: {:#x?}", spi.UniqueProcessId);
+            println!("image name: {:#x?}", proc_name);
+            println!("process id: {}", proc_id.0);
+
+            // そのプロセスがロックしているファイルを列挙する
+            let proc_hand = OpenProcess(PROCESS_ALL_ACCESS, 0, proc_id.0 as u32);
+            println!("proc_hand: {:#x?}", proc_hand);
+
+            if proc_hand == 0 as *mut c_void {
+                println!("OpenProcess failed");
+                continue;
+            }
+
+            
+
+            baseaddress = next_address as *mut c_void;
+        }   
+
+        VirtualFree(baseaddress, 0x0, 0x8000);
+    }
+}
