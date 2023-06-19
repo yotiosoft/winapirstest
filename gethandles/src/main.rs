ec55b0ac5710d423025d6ac79bed8ac063eddd71
YotioSoft <ytani0323@gmail.com>
2023-06-20 01:34:26 +0900

    SYSTEM_HANDLE_INFORMATION -> SYSTEM_HANDLE_INFORMATION_EX

--- gethandles/src/main.rs
+++ gethandles/src/main.rs
@@ -7,13 +7,8 @@ use winapi::um::winnt::{HANDLE,
     MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_EXECUTE_READWRITE, PROCESS_ALL_ACCESS
 };
 use ntapi::ntexapi::*;
-use ntapi::ntexapi::SYSTEM_HANDLE_TABLE_ENTRY_INFO;
-
-#[repr(C)]
-pub struct SYSTEM_HANDLE_INFORMATION {
-    pub number_of_handles: u32,
-    pub handles: [SYSTEM_HANDLE_TABLE_ENTRY_INFO; 10000],
-}
+use ntapi::ntexapi::SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX;
+use ntapi::ntexapi::SystemExtendedHandleInformation;
 
 fn FillStructureFromMemory<T>(struct_ptr: &mut T, memory_ptr: *const c_void, process_handle: *mut c_void) {
     unsafe {
@@ -31,7 +26,7 @@ fn main() {
 
         let mut tries = 0;
         loop {
-            let res = NtQuerySystemInformation(0x10, baseaddress, info_length, &mut info_length);
+            let res = NtQuerySystemInformation(SystemExtendedHandleInformation, baseaddress, info_length, &mut info_length);
 
             println!("res: {:x}", res);
             println!("info_length: {}", info_length);
@@ -53,22 +48,25 @@ fn main() {
 
         println!("baseaddress: {:x?}", baseaddress);
 
-        let mut spi: SYSTEM_HANDLE_INFORMATION = std::mem::zeroed();
+        let mut spi: SYSTEM_HANDLE_INFORMATION_EX = std::mem::zeroed();
         
         let a = GetCurrentProcess();
         FillStructureFromMemory(&mut spi, baseaddress as *const c_void, GetCurrentProcess());
 
-        println!("spi.NumberOfHandles: {}", spi.number_of_handles);
+        println!("spi.NumberOfHandles: {}", spi.NumberOfHandles);
 
-        for i in 0..spi.number_of_handles as usize {
+        // Handlesのオフセット
+        let offset = std::mem::size_of::<usize>() * 2;
+
+        for i in 0..spi.NumberOfHandles as usize {
             //if spi.Handles[i].UniqueProcessId as u32 == GetCurrentProcessId() {
             //    println!("Handle: {:#x?} pid: {:#x}", spi.handles[i].HandleValue, spi.handles[i].UniqueProcessId);
             //}
 
-            let mut handle: SYSTEM_HANDLE_TABLE_ENTRY_INFO = std::mem::zeroed();
-            FillStructureFromMemory(&mut handle, (baseaddress as usize + (8 + i * std::mem::size_of::<SYSTEM_HANDLE_TABLE_ENTRY_INFO>())) as *const c_void, GetCurrentProcess());
+            let mut handle: SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX = std::mem::zeroed();
+            FillStructureFromMemory(&mut handle, (baseaddress as usize + (offset + i * std::mem::size_of::<SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX>())) as *const c_void, GetCurrentProcess());
             if handle.UniqueProcessId as u32 == GetCurrentProcessId() {
-                println!("Handle: {:#x?} pid: {:#x}", handle.HandleValue, handle.UniqueProcessId);
+                println!("Handle: {:#x?} pid: {:#x}", handle.Object, handle.UniqueProcessId);
             }
         }
 
